f6f96db8be14f7d85f83c05ebd8ea3f3ba0e2d68
shohan <shohan.ece17@gmail.com>
2024-01-17 20:03:08 +0600

    continue from add task section

--- tasker/src/App.jsx
+++ tasker/src/App.jsx
@@ -1,9 +1,17 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
     </>
   );
 }

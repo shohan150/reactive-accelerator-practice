38d470bbad0b7c39de7821a22cd28a01936ee7d2
shohan <shohan.ece17@gmail.com>
2024-01-24 03:53:59 +0600

    cinerental started

--- cine-rental/tailwind.config.js
+++ cine-rental/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // we are declaring a custom tailwaind class named 'container' with the following properties.
+      container: {
+        center: true,
+        padding: "1.25 rem",
+      },
+      //setting up a primary color. Now, use this color into html elements(where color names are used in tailwind like red, green, pink etc.). Now, we have a new color named primary.
+      colors: {
+        primary: "#00d991",
+      },
+    },
   },
   plugins: [],
 };

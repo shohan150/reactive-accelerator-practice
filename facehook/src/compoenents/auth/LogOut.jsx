a42bf998e23d0ba1d205005e46abb43d10435da0
shohan <shohan.ece17@gmail.com>
2024-03-02 23:08:27 +0600

    setting

---
+++ facehook/src/compoenents/auth/LogOut.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import LogoutIcon from "../../assets/icons/logout.svg";
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
+  return (
+    <button className="icon-btn" onClick={handleLogout}>
+      <img src={LogoutIcon} alt="Logout" />
+    </button>
+  );
+};
+
+export default Logout;
